2229fcacb05b4439fc7c7f096972bc79f5912ac5
Shyam Sunder Verma <shyam@readybytes.in>
2014-04-03 12:21:03 +0530

    Download and source the aliases
    It will only download if aliases does not exist

--- import.sh
+++ import.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+mkdir /tmp/alias
+cd  /tmp/alias
+
+# download & unzip if not exist
+[ -f master.zip ] || ( wget https://github.com/readybytes/aliases/archive/master.zip && unzip master.zip )
+
+#source it
+source aliases-master/bash
+source aliases-master/git
