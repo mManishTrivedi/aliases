738afa7bb058d846b0e3399c450216579b7bab75
Shyam Sunder Verma <shyam@readybytes.in>
2014-05-08 08:16:10 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,2 +1,2 @@
-alias apt-proxy-unset='mv /etc/apt/apt.conf.d/01proxy ~/apt-proxy-file'
-alias apt-proxy-set='mv ~/apt-proxy-file /etc/apt/apt.conf.d/01proxy '
+alias apt-proxy-unset='sudo mv /etc/apt/apt.conf.d/01proxy ~/apt-proxy-file'
+alias apt-proxy-set='sudo mv ~/apt-proxy-file /etc/apt/apt.conf.d/01proxy '
