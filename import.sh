3be8821167d46088d0765ececf2b03fd7fc3a398
Shyam Sunder Verma <shyam@readybytes.in>
2014-09-17 12:20:07 +0530

    Update import.sh

--- import.sh
+++ import.sh
@@ -6,5 +6,5 @@ cd  /tmp/alias
 [ -f master.zip ] || ( wget https://github.com/readybytes/aliases/archive/master.zip && unzip master.zip )
 
 #source it
-source aliases-master/bash
-source aliases-master/git
+source /tmp/alias/aliases-master/bash
+source /tmp/alias/aliases-master/git
