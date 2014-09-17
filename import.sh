325ecfc67b4c5781fffc6c5460b5df43466b2352
Shyam Sunder Verma <shyam@readybytes.in>
2014-09-17 12:35:08 +0530

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-mkdir /tmp/alias
+echo "1. Checking /tmp/alias exist !"
+[ -d /tmp/alias ] || ( mkdir /tmp/alias )
 cd  /tmp/alias
 
+echo " 2. Downloading aliases "
 # download & unzip if not exist
 [ -f master.zip ] || ( wget https://github.com/readybytes/aliases/archive/master.zip && unzip master.zip )
 
 #source it
+echo " 3. Applying aliases "
 source /tmp/alias/aliases-master/bash
 source /tmp/alias/aliases-master/git
